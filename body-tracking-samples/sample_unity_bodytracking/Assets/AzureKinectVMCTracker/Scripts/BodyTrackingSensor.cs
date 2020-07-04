ed80a766ff11fdb3b3a2c267f40944e67ef88fb5
drumath <2237k.drumath@gmail.com>
2020-07-04 15:51:17 +0900

    :sparkles: とりあえずmain.csを模倣してセンサー処理を書いている

---
+++ body-tracking-samples/sample_unity_bodytracking/Assets/AzureKinectVMCTracker/Scripts/BodyTrackingSensor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace AzureKinectVMCTracker
+{
+    [RequireComponent(typeof(ConfigLoader))]
+    public class BodyTrackingSensor : MonoBehaviour
+    {
+        private BackgroundDataProvider m_backgroudProvider;
+        private BackgroundData m_backgroudData;
+
+        [SerializeField] private Transform _head, _leftHand, _rightHand, _hip, _leftFoot, _rightFoot;
+
+        private void Start()
+        {
+            SkeletalTrackingProvider m_skeletalTracking = new SkeletalTrackingProvider();
+
+            const int TRACKING_ID = 0;
+            m_skeletalTracking.StartClientThread(TRACKING_ID);
+            m_backgroudProvider = m_skeletalTracking;
+        }
+
+        private void Update()
+        {
+            if (m_backgroudProvider.IsRunning)
+            {
+                if (m_backgroudProvider.GetCurrentFrameData(ref m_backgroudData))
+                {
+//                    m_backgroudData.Bodies[0].
+                }
+            }
+        }
+
+        private void OnApplicationQuit()
+        {
+            m_backgroudProvider?.StopClientThread();
+        }
+    }
+}
