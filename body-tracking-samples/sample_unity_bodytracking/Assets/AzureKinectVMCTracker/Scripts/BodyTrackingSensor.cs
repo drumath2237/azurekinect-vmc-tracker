43509db094270da5ed152af802d21fd8d179c698
drumath <2237k.drumath@gmail.com>
2020-07-05 01:41:49 +0900

    :tada: 仮想トラッカーとして送信出来たので勝ち
    手の回転は考慮してないです

--- body-tracking-samples/sample_unity_bodytracking/Assets/AzureKinectVMCTracker/Scripts/BodyTrackingSensor.cs
+++ body-tracking-samples/sample_unity_bodytracking/Assets/AzureKinectVMCTracker/Scripts/BodyTrackingSensor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.Azure.Kinect.BodyTracking;
+using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 
 namespace AzureKinectVMCTracker
@@ -14,6 +15,12 @@ namespace AzureKinectVMCTracker
         [SerializeField] private Transform _head, _leftHand, _rightHand, _hip, _leftFoot, _rightFoot;
 
         private Dictionary<Transform, JointId> _jointTable;
+
+        [SerializeField] private float rotation_x_bias;
+        [SerializeField] private float rotation_y_bias;
+        [SerializeField] private float rotation_z_bias;
+        [SerializeField] private float rotation_w_bias;
+
 
         private void Start()
         {
@@ -45,10 +52,30 @@ namespace AzureKinectVMCTracker
                         foreach (var tracker in _jointTable)
                         {
                             tracker.Key.position = new Vector3(
-                                _mBackgroudData.Bodies[0].JointPositions3D[(int) tracker.Value].X,
-                                -_mBackgroudData.Bodies[0].JointPositions3D[(int) tracker.Value].Y,
-                                _mBackgroudData.Bodies[0].JointPositions3D[(int) tracker.Value].Z
-                            )*10f;
+                                                       -_mBackgroudData.Bodies[0].JointPositions3D[(int) tracker.Value]
+                                                           .X,
+                                                       -_mBackgroudData.Bodies[0].JointPositions3D[(int) tracker.Value]
+                                                           .Y,
+                                                       -_mBackgroudData.Bodies[0].JointPositions3D[(int) tracker.Value].Z
+                                                   ) * 10f;
+                            
+                            if (tracker.Value == JointId.HandLeft || tracker.Value == JointId.HandRight)
+                            {
+                                tracker.Key.rotation = Quaternion.LookRotation(tracker.Key.position - _hip.position);
+                                tracker.Key.Rotate(new Vector3(0,0,1), 90f);
+                                continue;
+                            }
+
+                            tracker.Key.rotation = new Quaternion(
+                                                       _mBackgroudData.Bodies[0].JointRotations[(int) tracker.Value].X *
+                                                       rotation_x_bias,
+                                                       _mBackgroudData.Bodies[0].JointRotations[(int) tracker.Value].Y *
+                                                       rotation_y_bias,
+                                                       _mBackgroudData.Bodies[0].JointRotations[(int) tracker.Value].Z *
+                                                       rotation_z_bias,
+                                                       _mBackgroudData.Bodies[0].JointRotations[(int) tracker.Value].W *
+                                                       rotation_w_bias
+                                                   ) * Quaternion.AngleAxis(180, new Vector3(0, 1, 0));
                         }
                     }
                 }
