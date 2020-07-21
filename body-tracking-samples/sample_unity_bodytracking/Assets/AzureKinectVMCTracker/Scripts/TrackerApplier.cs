f651912189b6a971c0043809614d8d4e1f325b34
drumath <2237k.drumath@gmail.com>
2020-07-21 18:28:08 +0900

    :sparkles: デバッグ用に回転を適用するスクリプトを作った

---
+++ body-tracking-samples/sample_unity_bodytracking/Assets/AzureKinectVMCTracker/Scripts/TrackerApplier.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+namespace AzureKinectVMCTracker
+{
+    public class TrackerApplier : MonoBehaviour
+    {
+        private Animator _avatar;
+
+        [SerializeField] private Transform _head;
+        [SerializeField] private Transform _leftHand;
+        [SerializeField] private Transform _righthand;
+        [SerializeField] private Transform _Pelvis;
+        [SerializeField] private Transform _leftFoot;
+        [SerializeField] private Transform _rightFoot;
+
+        private void Start()
+        {
+            _avatar = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            _avatar.GetBoneTransform(HumanBodyBones.Head).rotation = _head.rotation;
+            _avatar.GetBoneTransform(HumanBodyBones.Spine).rotation = _Pelvis.rotation;
+        }
+
+        void SetIK(AvatarIKGoal goal, Transform transform)
+        {
+            // _avatar.SetIKPosition(goal, transform.position);
+            // _avatar.SetIKPositionWeight(goal, 1.0f);
+            _avatar.SetIKRotation(goal, transform.rotation);
+            _avatar.SetIKRotationWeight(goal, 1.0f);
+        }
+
+        private void OnAnimatorIK(int layerIndex)
+        {
+            SetIK(AvatarIKGoal.LeftHand, _leftHand);
+            SetIK(AvatarIKGoal.RightHand, _righthand);
+            SetIK(AvatarIKGoal.LeftFoot, _leftFoot);
+            SetIK(AvatarIKGoal.RightFoot, _rightFoot);
+        }
+    }
+}
